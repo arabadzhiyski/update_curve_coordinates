7b4eb232ca2d1c4dc45ff1e9595db5d55653b3c5
Hristo Arabadzhiyski <hello@hristo.one>
2021-06-07 12:31:21 +0200

    Fixed exception for switch node.

--- onInputChanged.py
+++ onInputChanged.py
@@ -6,10 +6,9 @@ inputs= this_node.inputs()
 
 if (inputs):
     path = this_node.path() + "/check_input_type" # Is this the best way?
+    
     try:
-        if hou.node(path + "/check_input_type").cook(): # A bit hackish
-            hou.parm(path + "/switch/input").set(1)
-        else:
-            hou.parm(path + "/switch/input").set(0) # Set switch to error branch so HDA won't output anything.
+        hou.parm(path + "/switch/input").set(1) # (Re)set the switch
+        hou.node(path + "/check_input_type").cook() # Check input type
     except:
-        None # Don't throw exception to the consol. It's distracting.
+        hou.parm(path + "/switch/input").set(0) # Set switch to error stream
