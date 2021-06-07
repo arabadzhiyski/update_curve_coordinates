02ce6aa41dafeb3caedcbb211fd6aa58bcbe6d3d
Hristo Arabadzhiyski <hello@hristo.one>
2021-06-07 12:06:29 +0200

    Switch to error branch to prevent output.

--- onInputChanged.py
+++ onInputChanged.py
@@ -7,6 +7,9 @@ inputs= this_node.inputs()
 if (inputs):
     path = this_node.path() + "/check_input_type" # Is this the best way?
     try:
-        hou.node(path).cook()
+        if hou.node(path + "/check_input_type").cook(): # A bit hackish
+            hou.parm(path + "/switch/input").set(1)
+        else:
+            hou.parm(path + "/switch/input").set(0) # Set switch to error branch so HDA won't output anything.
     except:
         None # Don't throw exception to the consol. It's distracting.
